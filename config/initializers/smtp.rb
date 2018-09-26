946f45b11b450861e15b71df1dc18657cf58b030
Fabio Souza <fabiobarboza7@hotmail.com>
2018-09-26 13:29:33 -0300

    smtp error

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   address: "smtpout.secureserver.net",
   port: 465,
   domain: 'moodev.com.br',
-  user_name: ENV['SERVER_ADDRESS'],
-  password: ENV['SERVER_PASSWORD'],
+  user_name: "pelotas@moodev.com.br",
+  password: "Channel3@",
   authentication: :login,
   ssl: true
   # enable_starttls_auto: true
