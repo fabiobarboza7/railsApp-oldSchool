f68fa202fcd569c8025820abfd11f91380cce586
Fabio Souza <fabiobarboza7@hotmail.com>
2018-09-28 07:05:16 -0300

    adding user fiels first_name last_name age, photo phone

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180928075609) do
+ActiveRecord::Schema.define(version: 20180928100421) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,12 @@ ActiveRecord::Schema.define(version: 20180928075609) do
     t.datetime "remember_created_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "photo"
+    t.string   "photo_cache"
+    t.string   "age"
+    t.string   "phone"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
