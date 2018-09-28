74b6e0ab4632338c521b2c1817be2d27ca7a98d6
Fabio Souza <fabiobarboza7@hotmail.com>
2018-09-28 17:04:28 -0300

    ajax new subjects and ppolicies

---
+++ app/policies/subject_policy.rb
@@ -0,0 +1,24 @@
+class SubjectPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def index?
+    show?
+  end
+
+  def show?
+    true
+  end
+
+  def new?
+    create?
+  end
+
+  def create?
+    true
+  end
+
+end
