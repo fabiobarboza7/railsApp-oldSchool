3dbbdf79391ffedc54ceb97e70e7546f85f6d27e
Fabio Souza <fabiobarboza7@hotmail.com>
2018-09-28 04:14:28 -0300

    pundit authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,26 @@ class ApplicationController < ActionController::Base
   add_flash_types :success
   protect_from_forgery with: :exception
 
+   before_action :authenticate_user!
+  include Pundit
+
+  # Pundit: white-list approach.
+  after_action :verify_authorized, except: :index, unless: :skip_pundit?
+  after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+
+  # Uncomment when you *really understand* Pundit!
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  def user_not_authorized
+    flash[:alert] = "Você não está autorizado a realizar esta ação"
+    redirect_to(root_path)
+  end
+
+  private
+
+  def skip_pundit?
+    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+  end
+
   helper_method :resource_name, :resource, :devise_mapping, :resource_class
 
   def resource_name
