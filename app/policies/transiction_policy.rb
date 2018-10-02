eff6d7ee0cdf2837c2c600f6307f4a3553491ee4
Fabio Souza <fabiobarboza7@hotmail.com>
2018-10-02 05:33:17 -0300

    wallet deeplook

---
+++ app/policies/transiction_policy.rb
@@ -0,0 +1,29 @@
+class TransictionPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def index?
+    show?
+  end
+
+  def show?
+    true
+  end
+
+  def new?
+    create?
+  end
+
+  def create?
+    true
+  end
+
+  def update?
+    true
+  end
+
+
+end
