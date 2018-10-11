481550d8fd82658a757a1769c0fa1c042312bc36
Fabio Souza <fabiobarboza7@hotmail.com>
2018-10-11 02:46:37 -0300

    charts

--- app/helpers/dashboards_helper.rb
+++ app/helpers/dashboards_helper.rb
@@ -1,5 +1,16 @@
 module DashboardsHelper
+
   def number_to_currency_br(number)
     number_to_currency(number, :unit => "m$ ", :separator => ",", :delimiter => ".")
   end
+
+  def user_range
+    user_answers = UserAnswer.where(user: current_user)
+    correct = user_answers.where(is_true: true)
+    wrong = user_answers.where(is_true: false)
+
+    users = [correct, wrong]
+    return users
+  end
+
 end
