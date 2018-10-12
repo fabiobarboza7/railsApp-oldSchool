0a1c6db5f60456297291fc9480e367a5a73eaa3d
Fabio Souza <fabiobarboza7@hotmail.com>
2018-10-12 02:08:08 -0300

    routes path rename

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
-  resources :courses, except: [:index, :edit, :update, :destroy] do
-    resources :subjects, except: [:destroy] do
-      resources :lessons, except: [:destroy] do
-        resources :quizzes, except: [:destroy] do
-          resources :questions, except: [:destroy]
+  resources :courses, except: [:index, :edit, :update, :destroy], :path => "curso" do
+    resources :subjects, except: [:destroy], :path => "materia" do
+      resources :lessons, except: [:destroy], :path => "aula" do
+        resources :quizzes, except: [:destroy], :path => "quiz" do
+          resources :questions, except: [:destroy], :path => "questao"
         end
       end
     end
@@ -14,14 +14,14 @@ Rails.application.routes.draw do
   resources :user_answers, only: [:new, :create]
   resources :transictions, only: [:new, :create, :index, :show]
   resources :users, only: [] do
-    resources :wallets, only: [:show]
+    resources :wallets, only: [:show], :path => "carteira"
   end
-  resources :test_classes, except: [:edit, :update, :destroy]
-  resources :contacts, except: [:edit, :update, :destroy]
-  resources :blogs, except: [:destroy]
+  resources :test_classes, except: [:edit, :update, :destroy], :path => "aula-gratis"
+  resources :contacts, except: [:edit, :update, :destroy], :path => "contato"
+  resources :blogs, except: [:destroy], :path => "blog"
   resources :dashboards, only: [:index]
 
-  devise_for :users
+  devise_for :users, :path => "usuario"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'pages/home'
