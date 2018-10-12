ca810c6e33afab7078d296b5397a6c5f6a819a9e
Fabio Souza <fabiobarboza7@hotmail.com>
2018-10-12 01:58:53 -0300

    friendly implementation

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,3 +1,14 @@
 class Blog < ApplicationRecord
   mount_uploader :photo, PhotoUploader
+
+  extend FriendlyId
+  friendly_id :slug_candidates, use: :slugged
+
+  def slug_candidates
+    [
+      :title,
+      [:title, :id]
+    ]
+  end
+
 end
