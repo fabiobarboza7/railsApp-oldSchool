168e54cd2a8b30f62eb6480b700048715e3cec32
Fabio Souza <fabiobarboza7@hotmail.com>
2018-10-23 18:14:39 -0300

    fixing comma error in show lesson, fixing routes name

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,7 +2,7 @@ class Lesson < ApplicationRecord
   belongs_to :subject
   has_many :quizzes
   has_many :tests
-
+  # before_save :file_nil?
   mount_uploader :file, PhotoUploader
 
   def next
@@ -12,6 +12,12 @@ class Lesson < ApplicationRecord
   def prev
     subject.lessons.where("id < ?", id).last
   end
+
+  # def file_nil?
+  #   if self.file === "" || self.file === " "
+  #     self.file = nil
+  #   end
+  # end
 
 
 end
