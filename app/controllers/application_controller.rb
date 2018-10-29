f2c84a58b311fbc56973268b26076782c4d4b6ad
Fabio Souza <fabiobarboza7@hotmail.com>
2018-10-29 11:47:31 -0300

    setup rails meta tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,10 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
 
+  def default_url_options
+    { host: ENV["HOST"] || "localhost:3000" }
+  end
+
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :first_name, :last_name, :age, :date_of_birth, :photo, :photo_cache, :admin])
