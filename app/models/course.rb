824f64c1b92418838c693e72e3735447c60833e6
Fabio Souza <fabiosouza@MacBook-Pro-de-Fabio-2.local>
2018-12-04 14:17:23 -0200

    url name

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,14 @@ class Course < ApplicationRecord
   has_many  :subjects, dependent: :destroy
   mount_uploader :photo, PhotoUploader
 
+  extend FriendlyId
+  friendly_id :slug_candidates, use: :slugged
+
+  def slug_candidates
+    [
+      :title,
+      [:title, :id]
+    ]
+  end
+
 end
