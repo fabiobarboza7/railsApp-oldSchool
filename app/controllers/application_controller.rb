136041e6f31a48c97ce4931de823407c9e40b60e
Fabio Souza <fabiosouza@MacBook-Pro-de-Fabio-2.local>
2019-01-20 23:08:40 -0200

    trying fixing unauthorized user sign_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -66,18 +66,18 @@ class ApplicationController < ActionController::Base
     root_path
   end
 
-  # protected
+  protected
 
-  # def authenticate_user!
-  #   if user_signed_in?
-  #     super
-  #   else
-  #     redirect_to root_path
-  #     flash[:alert] = "Tivemos algum problema, tente novamente..."
-  #     ## if you want render 404 page
-  #     ## render :file => File.join(Rails.root, 'public/404'), :formats => [:html], :status => 404, :layout => false
-  #   end
-  # end
+  def authenticate_user!
+    if user_signed_in?
+      super
+    else
+      redirect_to root_path
+      flash[:alert] = "Tivemos algum problema, tente novamente..."
+      ## if you want render 404 page
+      ## render :file => File.join(Rails.root, 'public/404'), :formats => [:html], :status => 404, :layout => false
+    end
+  end
 
 
 end
