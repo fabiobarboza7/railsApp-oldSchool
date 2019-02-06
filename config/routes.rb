edff483257d40ca2afa613e8f38247f364484140
Fabio Souza <fabiosouza@MBP-de-Fabio-2.HotSpot300>
2019-02-06 17:12:13 -0200

    franchising page

--- config/routes.rb
+++ config/routes.rb
@@ -27,7 +27,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'pages/home'
   get '/rio-de-janeiro', to: "pages#rio_de_janeiro"
-  get '/seja-um-franquado', to: "pages#franchise"
+  get '/seja-um-franqueado', to: "pages#franchise"
   root to: 'pages#home'
 
 end
