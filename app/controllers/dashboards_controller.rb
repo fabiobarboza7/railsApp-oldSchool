5d8caa160dba8d6136859a1952ee32d302a665e9
Fábio Barboza Coelho de Souza <fabiobarboza7@hotmail.com>
2019-07-12 14:26:16 -0300

    fixiing user positions on ranking

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -11,23 +11,7 @@ class DashboardsController < ApplicationController
       @user = current_user
       @enrollments = current_user.enrollments
       @users = User.all.order(score: :desc)
-      @user_best = @users.first
-      @user_second = @users[1...2]
-      @user_third = @users[2...3]
-      @user_fourth = @users[3...4]
-
-      if @user_best.score == @user_second.first.score
-        @user_second = @users[2...3]
-      end
-
-      if @user_second.first.score == @user_third.first.score
-        @user_third = @users[3...4]
-      end
-
-      if @user_third.first.score == @user_fourth.first.score
-        @user_fourth = @users[4...5]
-      end
-
+      
       @quizzes = Quiz.all
       @files = Attachment.where(user: current_user)
       @enroll = Enrollment.where(user: current_user.id)
